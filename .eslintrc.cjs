b8fdc4faa703305285860eab9aff74e31808ec21
정서영 <sksdu_4644@partner.skshieldus.com>
2026-05-08 10:05:43 +0900

    ESLint 플러그인  javascript -> typescript 형식 설정으로 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,30 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-    root: true,
-    env: {
-        node: true
-    },
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
-    parserOptions: {
-        ecmaVersion: 'latest'
-    },
-    rules: {
-        'vue/multi-word-component-names': 'off',
-        'vue/no-reserved-component-names': 'off',
-        'vue/component-tags-order': [
-            'error',
-            {
-                order: ['script', 'template', 'style']
-            }
-        ]
-    }
-};
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    // 기존 규칙
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style']
+      }
+    ]
+  }
+}
