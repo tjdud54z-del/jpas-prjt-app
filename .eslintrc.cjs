6f9216b98fb19f60b7be05ad0289f46c3627dc62
정서영 <sksdu_4644@partner.skshieldus.com>
2026-05-13 17:03:34 +0900

    eslint, prettierrc json 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,26 +3,64 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es2022: true
   },
+
+  /**
+   * Vue SFC 파서
+   */
   parser: 'vue-eslint-parser',
+
   parserOptions: {
+    /**
+     * <script setup lang="ts"> 내부는 TS 파서
+     */
     parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    extraFileExtensions: ['.vue']
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+
+  extends: [
+    'eslint:recommended',
+
+    // Vue 3 권장 규칙
+    'plugin:vue/vue3-recommended',
+
+    // Prettier와 충돌 제거
+    '@vue/eslint-config-prettier'
+  ],
+
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto'
-      }
-    ],
-    // 기존 규칙 유지
+    /**
+     * Prettier를 ESLint 에러로 표시
+     */
+    'prettier/prettier': 'off',
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     endOfLine: 'auto'
+    //   }
+    // ],
+
+    /* =========================
+       Vue 템플릿 관련 (Prettier에 위임)
+    ========================= */
+
+    // 속성 줄바꿈/정렬은 Prettier에게 맡김
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+
+    // 컴포넌트 이름 정책 완화
     'vue/multi-word-component-names': 'off',
     'vue/no-reserved-component-names': 'off',
+
+    // script / template / style 순서
     'vue/component-tags-order': [
       'error',
       {
